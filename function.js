089c6e1414800f55bd94741b8f54c672088b7cf1
Elensn <35395765+Elensn@users.noreply.github.com>
2021-07-13 08:16:51 +0200

    Update function.js

--- function.js
+++ function.js
@@ -4,3 +4,14 @@ exports.myfunction = (event, context) => {
         'data': 'BrTestingBranch was used!',
     };
 }
+
+exports.printUtf8 = (event, context) => {
+    var message = 'text';
+    console.log(utf8.encode(message));
+
+    return {
+        'status': 200,
+        'data': 'OK',
+        
+    };
+};
